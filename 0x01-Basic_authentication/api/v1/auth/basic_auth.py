94a8fa54d06bd2766d4dc72816c74f6327978a86
AzorAha1 <wizfaiz@icloud.com>
2024-05-21 22:54:12 +0100

    basic auth added

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""sumary_line
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """_summary_
+
+    Args:
+        Auth (_type_): _description_
+    """
+    pass
