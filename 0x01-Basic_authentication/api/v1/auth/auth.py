04042732e842372da3ef442ca64160468553cfd4
AzorAha1 <wizfaiz@icloud.com>
2024-05-21 01:02:01 +0100

    authentication added

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,20 @@ class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth public method"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
+        if not path.endswith('/'):
+            path = path + '/'
+        for excluded_path in excluded_paths:
+            if not excluded_path.endswith('/'):
+                excluded_path = excluded_path + '/'
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header"""
