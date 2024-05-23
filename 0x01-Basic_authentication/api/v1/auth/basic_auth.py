0f525edc4d15aa72c34c0149c176002b02186fcd
AzorAha1 <wizfaiz@icloud.com>
2024-05-23 03:05:09 +0100

    basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -90,6 +90,6 @@ class BasicAuth(Auth):
         if not users:
             return None
         for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-        return None
+            if not user.is_valid_password(user_pwd):
+                return None
+        return user
