ac09a8a71d4c80b6ba0eab6436a7ee19ca33c26e
AzorAha1 <wizfaiz@icloud.com>
2024-05-26 01:04:18 +0100

    session auth updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,19 @@
 """this will be a class"""
 
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """session auth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        else:
+            sessionid = str(uuid4())
+            self.user_id_by_session_id[sessionid] = user_id
+            return sessionid
