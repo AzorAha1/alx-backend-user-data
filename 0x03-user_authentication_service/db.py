c0ac3350dd6bcb3888014535a6e1e9440678460f
AzorAha1 <wizfaiz@icloud.com>
2024-05-28 20:39:36 +0100

    db update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,7 +42,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """filter user"""
         try:
             firstuser = self.__session.query(User).filter_by(**kwargs).one()
