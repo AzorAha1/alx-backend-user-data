0410368adac1f4a28662a553a75eaefe9db5bf79
AzorAha1 <wizfaiz@icloud.com>
2024-05-29 02:27:41 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,3 +45,14 @@ class Auth:
             return self._db.add_user(email=email, hashed_password=hashpassword)
         else:
             raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """valid login"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
