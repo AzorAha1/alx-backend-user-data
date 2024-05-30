6ef7e332c5a417c490b03560fffb4a270b09ed77
AzorAha1 <wizfaiz@icloud.com>
2024-05-30 23:27:27 +0100

    auth update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -102,5 +102,6 @@ class Auth:
     def destroy_session(self, user_id: int):
         """destroy session"""
         user = self._db._session.query(User).filter_by(user_id=user_id).first()
-        user.session_id = None
-        self._db._session.commit()
+        if user:
+            user.session_id = None
+            self._db._session.commit()
